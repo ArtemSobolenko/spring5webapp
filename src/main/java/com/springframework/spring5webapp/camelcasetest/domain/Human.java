d2b2109cd1beaf501a91eb5322af6b31ec87c750
ArtemSobolenko <artem.sobolenko@gmail.com>
2020-05-22 12:07:20 +0300

    Human modified add

--- src/main/java/com/springframework/spring5webapp/camelcasetest/domain/Human.java
+++ src/main/java/com/springframework/spring5webapp/camelcasetest/domain/Human.java
@@ -3,6 +3,8 @@ package com.springframework.spring5webapp.camelcasetest.domain;
 public class Human {
     private int eye;
 
+    private int head;
+
     public void showEyeCount(){
         System.out.println(eye);
     }
