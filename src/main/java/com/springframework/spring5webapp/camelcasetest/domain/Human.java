34fe2a11bf9185df16c041b2163a12b41c80f370
ArtemSobolenko <artem.sobolenko@gmail.com>
2020-05-22 12:05:23 +0300

    function show eye added

--- src/main/java/com/springframework/spring5webapp/camelcasetest/domain/Human.java
+++ src/main/java/com/springframework/spring5webapp/camelcasetest/domain/Human.java
@@ -2,4 +2,8 @@ package com.springframework.spring5webapp.camelcasetest.domain;
 
 public class Human {
     private int eye;
+
+    public void showEyeCount(){
+        System.out.println(eye);
+    }
 }
